36b62fbc031a6bc4719d25f1c739592af6122812
bosshentai <baptistamhernani@gmail.com>
2021-02-09 22:08:50 -0100

    learning about strings

---
+++ Python_basics/string.py
@@ -0,0 +1,18 @@
+# print(type("hu hello there 24!"))
+# username = 'bosshentai'
+# password = 'supercode'
+# long_string = '''
+# WOW
+# O O
+# ---
+# '''
+
+# print(long_string)
+
+
+first_name = 'Hernani'
+last_name = 'Baptista'
+
+full_name =  first_name + ' ' + last_name
+
+print(full_name)
