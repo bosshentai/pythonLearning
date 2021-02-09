f2a9a7942b05f676d9cc6739725c4a8b7ddcf44c
bosshentai <baptistamhernani@gmail.com>
2021-02-09 21:31:02 -0100

    doing some example with print round abs

--- Python_basics/number.py
+++ Python_basics/number.py
@@ -1,3 +1,16 @@
+#integers 
+# 
+# print(type(2 + 3))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4))
 
 
-print(2 + 3)
+# print(2 ** 3)
+# print(5 // 4)
+# print(6 % 4)
+
+
+# math functions 
+print(round(3.5))
+print(abs(-20))
