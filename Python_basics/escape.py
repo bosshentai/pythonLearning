f72a2a784db287d088e80b4e8d8f53c73b52bbbb
bosshentai <baptistamhernani@gmail.com>
2021-02-09 22:33:01 -0100

    strings comands

---
+++ Python_basics/escape.py
@@ -0,0 +1,7 @@
+# Escape Sequence
+weather = "It's sunny"
+weater2 = 'It\'s \"Kind of\" sunny'
+weater3 = '\tIt\'s \"Kind of\" sunny \n \thope you have a good day!'
+
+print(weater2)
+print(weater3)
