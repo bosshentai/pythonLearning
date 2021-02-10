1f9149df2b5b753f668ae3a5a32f6573ac556959
bosshentai <baptistamhernani@gmail.com>
2021-02-10 13:13:21 -0100

    add file listMethods and some examples

---
+++ Python_basics/listMethods.py
@@ -0,0 +1,26 @@
+basket = [1,2,3,4,5]
+#len()
+print(len(basket))
+
+
+#adding
+# basket.append(100)
+# newList =  basket
+
+# basket.insert(5,100)
+# newList = basket
+
+#basket.extend([100,101])
+newList = basket.extend([100])
+#print(basket)
+#print(newList)
+
+#removing
+#basket.pop()
+#print(basket)
+
+#basket.remove(4)
+#print(basket)
+
+newList = basket.clear()
+print(basket)
