1bcdb90cf8c86515c19b1053f86bb5a6863da8ad
bosshentai <baptistamhernani@gmail.com>
2021-02-09 23:49:45 -0100

    index with string

---
+++ Python_basics/indexes.py
@@ -0,0 +1,20 @@
+selfish = '01234567'
+
+print (selfish[0])
+
+# [start:stop]
+print(selfish[0:2])
+
+# [start:stop:stepover]
+print(selfish[0:8:3])
+
+print(selfish[1:])
+
+# all string
+print(selfish[::1])
+
+# last index
+print(selfish[-1])
+
+# reverse index 
+print(selfish[::-1])
