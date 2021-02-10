1a9a3118c95b473c3f5196571ad141ecf47e7cae
bosshentai <baptistamhernani@gmail.com>
2021-02-10 00:24:19 -0100

    last example for indexes

--- Python_basics/indexes.py
+++ Python_basics/indexes.py
@@ -1,20 +1,25 @@
 selfish = '01234567'
 
-print (selfish[0])
+# print (selfish[0])
 
-# [start:stop]
-print(selfish[0:2])
+# # [start:stop]
+# print(selfish[0:2])
 
-# [start:stop:stepover]
-print(selfish[0:8:3])
+# # [start:stop:stepover]
+# print(selfish[0:8:3])
 
-print(selfish[1:])
+# print(selfish[1:])
 
-# all string
-print(selfish[::1])
+# # all string
+# print(selfish[::1])
 
-# last index
-print(selfish[-1])
+# # last index
+# print(selfish[-1])
 
-# reverse index 
-print(selfish[::-1])
+# # reverse index 
+# print(selfish[::-1])
+
+## Immutability
+selfish = 100
+
+print(selfish)
