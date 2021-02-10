1cf63722ebe287f58a30c279db40b06156385fe7
bosshentai <baptistamhernani@gmail.com>
2021-02-10 14:12:08 -0100

    last info about list

--- Python_basics/listMethods.py
+++ Python_basics/listMethods.py
@@ -22,5 +22,11 @@ newList = basket.extend([100])
 #basket.remove(4)
 #print(basket)
 
-newList = basket.clear()
-print(basket)
+# # newList = basket.clear()
+# print(basket)
+basket2 = ['a','b','c','d','e']
+#print(basket2.index('d'))
+#print( 'd' in basket2 )
+
+
+print(basket2.count('d'))
