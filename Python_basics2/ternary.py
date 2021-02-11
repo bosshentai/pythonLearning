00315c5fdb8f416d0331b54b39044165a7bc3143
bosshentai <baptistamhernani@gmail.com>
2021-02-11 21:18:54 -0100

    exemple ternary

---
+++ Python_basics2/ternary.py
@@ -0,0 +1,7 @@
+# Ternary  Operator 
+
+# condition_if_true if condtion else condition_if_false 
+isFriend = True 
+canMessage = "message allowed" if isFriend else "not allowed to message"
+
+print(canMessage)
