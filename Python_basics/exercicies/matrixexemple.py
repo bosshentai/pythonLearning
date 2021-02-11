4dee02b3dafeebc9b5e3e3dbc396a4ef5fc002f9
bosshentai <baptistamhernani@gmail.com>
2021-02-11 20:53:21 -0100

    exemple of matrix

---
+++ Python_basics/exercicies/matrixexemple.py
@@ -0,0 +1,15 @@
+numero = 1
+l = 1
+while (l <= 5):
+    numero = l
+    c = 1
+    while (c <= 4):
+        print(f'coluna {c}')
+        
+        c +=1
+    print(f'linha {numero}')
+    numero +=1
+    l += 1
+
+   
+
