116eac7061edacf9c85582a9db0bfcf5f7557bcb
bosshentai <baptistamhernani@gmail.com>
2021-02-11 22:07:49 -0100

    add 2 file for and is

---
+++ Python_basics2/for.py
@@ -0,0 +1,9 @@
+# for item in 'Zero to mastery':
+#     print(item)
+
+for item in [1,2,3,4,5]:
+    for x in ['a','b', 'c']:
+        print(item, x)
+
+# iterable - list, dictionary, tuple, set , string 
+# iterated -> one by one check each item in the collection. 
