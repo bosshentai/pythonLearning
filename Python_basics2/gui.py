18659adab0eee50a2b936de27f811d9d412215b5
bosshentai <baptistamhernani@gmail.com>
2021-02-12 09:41:04 -0100

    add some exemple gui while

---
+++ Python_basics2/gui.py
@@ -0,0 +1,21 @@
+# Exercise ! 
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+
+# if find 0 display ' '
+# if find 1 display .
+
+for row in picture:
+    for pixel in row:
+        if (pixel == 1):
+            print('*',end='')
+        else:
+            print(' ',end='')
+    print('')
--- Python_basics2/while.py
+++ Python_basics2/while.py
@@ -1,5 +1,22 @@
-i = 0 
+# i = 0 
 
-while i < 50:
-    print(i)
-    break
+# while i < 50:
+#     print(i)
+#     i += 1 
+# else:
+#     print('done with all the work')
+
+
+# myList = [1,2,3]
+# for item in myList:
+#     print(item)
+
+# i = 0 
+# while i < len(myList):
+#     print(myList[i])
+    # i += 1
+
+while True:
+    response = input('say something: ')
+    if (response == 'bye'):
+        break
