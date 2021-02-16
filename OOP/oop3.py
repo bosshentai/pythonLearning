dbbb8d68dedcb766a20d09455cba92dfec9c45d1
bosshentai <baptistamhernani@gmail.com>
2021-02-16 10:00:20 -0100

    add file oop4

--- OOP/oop3.py
+++ OOP/oop3.py
@@ -6,16 +6,20 @@
 
 # Inheritance  
 class User():
+    def __init__(self,email):
+        self.email = email
     def sign_in(self):
         print('logged in')
 
 
 class Wizard(User):
-    def __init__(self,name, power):
+    def __init__(self,name, power, email):
+        super().__init__(email) # super
         self.name = name
         self.power = power
 
     def attack(self):
+        
         print(f'attacking with power {self.power}')
 
 class Archer(User):
@@ -24,9 +28,22 @@ class Archer(User):
         self.numArrows = numArrows
 
     def attack(self):
+        
         print(f'attacking with arrows:arrows left- {self.numArrows}')
 
-wizard1 = Wizard('Merlin', 20)
+
+#introspection
+
+wizard1 = Wizard('Merlin', 20,'merlin@gmail.com')
 archer1 = Archer('Robin',50)
-wizard1.attack()
-archer1.attack()
+
+print(dir(wizard1))
+# wizard1.attack()
+# archer1.attack()
+
+#print(isinstance(wizard1,User))
+# polimorphism
+def playerAttack(char):
+    char.attack()
+
+playerAttack(wizard1)
