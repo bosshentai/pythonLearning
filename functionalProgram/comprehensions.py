191cf24b6cdc849edeb89178858249d43d4be203
bosshentai <baptistamhernani@gmail.com>
2021-02-18 11:48:47 -0100

    add comprehensions with list

---
+++ functionalProgram/comprehensions.py
@@ -0,0 +1,10 @@
+# list, set, dictionary
+
+myList = [char for char in 'hello']
+myList2 = [num for num in range(0,100)]
+myList3 = [ num*2 for num in range(0,100)]
+myList4 = [ num**2 for num in range(0,100) if num % 2 == 0] # even numbers
+
+
+print(myList)
+print(myList4)
