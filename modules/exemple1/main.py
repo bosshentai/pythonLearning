e4136926311f9391af0f7d7351f404787bf0efe7
bosshentai <baptistamhernani@gmail.com>
2021-02-19 11:35:27 -0100

    add exemples of modules

---
+++ modules/exemple1/main.py
@@ -0,0 +1,11 @@
+from utility import multiply, divide
+from  shopping.moreShopping.shoppingCart import buy
+
+if __name__ == '__main__':
+    print(buy('apple'))
+    print(divide(5,2))
+    print(multiply(5,2))
+    print(max([1,2,3]))
+
+
+
--- modules/main.py
+++
@@ -1,3 +0,0 @@
-import utility
-
-print(utility)
