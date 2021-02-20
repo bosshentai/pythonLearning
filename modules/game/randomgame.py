1958528cdedfddd7d18a36231d345c08abbd0972
bosshentai <baptistamhernani@gmail.com>
2021-02-20 10:51:54 -0100

    finish the random game

--- modules/game/randomgame.py
+++ modules/game/randomgame.py
@@ -1,2 +1,19 @@
-import sys 
+from random import randint
+import sys
+
+answer = randint(int(sys.argv[1]),int(sys.argv[2]))
+
+
+while True:
+    try:
+        guess = int(input('quess a number 1~10: '))
+        if 0 < guess < 11:
+            if guess == answer:
+                print('you are a genius')
+                break
+        else:
+            print('hey bozo, i said 1~10')
+    except ValueError:
+        print('please enter a number')
+        continue
 
