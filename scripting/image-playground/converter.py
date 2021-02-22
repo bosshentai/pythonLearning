57779f4361047841856f58906853e869d75706b0
bosshentai <baptistamhernani@gmail.com>
2021-02-22 13:03:52 -0100

    all changed in image-playground2

---
+++ scripting/image-playground/converter.py
@@ -0,0 +1,14 @@
+# convert JPG to PNG
+import sys 
+import os 
+from PIL import Image
+
+# grab first and second argument
+
+
+# check is new/ exist, if not created 
+
+
+# loop through Pokedex 
+# conert images to png 
+# save to the new folder 
--- scripting/image-playground2/image.py
+++ scripting/image-playground2/image.py
@@ -1,5 +1,10 @@
 from PIL import Image
 
+
 img = Image.open('./image/astro.jpg')
+
+#newImg = img.resize((400,400))
+img.thumbnail((400,400))
+img.save('imgchanged/thumbnail.jpg')
 
 print(img.size)
