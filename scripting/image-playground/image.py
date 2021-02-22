d6f00e7c08aa31168641c448aa55b9f0cb824dc1
bosshentai <baptistamhernani@gmail.com>
2021-02-22 12:51:14 -0100

    add imageplayground/changed the exemples of some change in a image

--- scripting/image-playground/image.py
+++ scripting/image-playground/image.py
@@ -1,7 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+
+# Pillow library 
+
 
 img = Image.open('./Pokedex/pikachu.jpg')
 
+#filteredImg = img.filter(ImageFilter.SMOOTH)
+filteredImg = img.convert('L')
+crooked = filteredImg.rotate(180)
+
+crooked.save('./changed/grey1.png','png')
+filteredImg.save('./changed/grey.png', 'png')
+
+resize = filteredImg.resize((300,300))
+resize.save('./changed/resize.png', 'png')
+
+
+box = (100,100,400,400)
+region = filteredImg.crop(box)
+region.save('./changed/region.png','png')
+
+#filteredImg.show()
 #print(img.format)
 #print(img.size)
 #print(img.mode)
