b3bff4a52c806da047666205378df9b174df6fdd
bosshentai <baptistamhernani@gmail.com>
2021-02-23 08:49:28 -0100

    converter for jpg to png done

--- scripting/image-playground/converter.py
+++ scripting/image-playground/converter.py
@@ -4,11 +4,25 @@ import os
 from PIL import Image
 
 # grab first and second argument
+imageFolder = sys.argv[1]
+outputFolder = sys.argv[2]
 
 
 # check is new/ exist, if not created 
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
+
+#print(imagemFolder,outputFolder)
+
 
 
 # loop through Pokedex 
+for filename in os.listdir(imageFolder):
+    img = Image.open(f'{imageFolder}{filename}')
+    cleanName = os.path.splitext(filename)[0]
+    #print(cleanName)
+    img.save(f'{outputFolder}{cleanName}.png', 'png')
+    # print('all done!')
+
 # conert images to png 
 # save to the new folder 
