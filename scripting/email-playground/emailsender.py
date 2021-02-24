ad215556cb2a835b08e6241c0d07c092d9b4db41
bosshentai <baptistamhernani@gmail.com>
2021-02-23 23:53:57 -0100

    finish email send

--- scripting/email-playground/emailsender.py
+++ scripting/email-playground/emailsender.py
@@ -3,12 +3,13 @@ from email.message import EmailMessage
 from string import Template
 from  pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Hernani Baptista'
 email['to'] = 'baptistamhernani@gmail.com'
 email['subject'] = 'Pegam na ov'
 
-email.set_content('I am learning Python ')
+email.set_content(html.substitute({'name' : 'Tintin'}),'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
